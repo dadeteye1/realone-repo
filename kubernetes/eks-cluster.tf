0dbbb88ce5dca15304bb05780da26105b828e50b
dadeteye1 <108375678+dadeteye1@users.noreply.github.com>
2024-05-13 09:35:04 +0100

    Update eks-cluster.tf

--- kubernetes/eks-cluster.tf
+++ kubernetes/eks-cluster.tf
@@ -1,25 +1,29 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 19.0"
+  version = "~> 20.0"  # Update to the new version
+
   cluster_endpoint_public_access  = true
+
   cluster_addons = {
     coredns = {
-      most_recent = true
+      most_recent                    = true
+      resolve_conflicts_on_create    = "OVERWRITE"
     }
     kube-proxy = {
-      most_recent = true
+      most_recent                    = true
+      resolve_conflicts_on_create    = "OVERWRITE"
     }
     vpc-cni = {
-      most_recent = true
+      most_recent                    = true
+      resolve_conflicts_on_create    = "OVERWRITE"
     }
   }
-  
 
-  cluster_name = "myAppp-eks-cluster"  
-  cluster_version = "1.27"
+  cluster_name    = "myAppp-eks-cluster"
+  cluster_version = "1.27"  # Update if a new Kubernetes version is available
 
   subnet_ids = module.myAppp-vpc.private_subnets
-  vpc_id = module.myAppp-vpc.vpc_id
+  vpc_id     = module.myAppp-vpc.vpc_id
 
   tags = {
     environment = "development"
@@ -28,12 +32,12 @@ module "eks" {
 
   eks_managed_node_groups = {
     dev = {
-      min_size     = 1
-      max_size     = 6
-      desired_size = 3
-
+      min_size       = 1
+      max_size       = 6
+      desired_size   = 3
       instance_types = ["t2.small"]
-      key_name       = "devopskeypair"
-    }
+      key_name       = "ohio"
   }
+}
+
 }
